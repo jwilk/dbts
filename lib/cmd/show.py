9420323481d95d30098807c35a39ebcfde452c7a
Jakub Wilk <jwilk@jwilk.net>
2015-09-19 23:55:35 +0200

    debsoap: move most Debian BTS SOAP logic here.

--- lib/cmd/show.py
+++ lib/cmd/show.py
@@ -21,14 +21,13 @@
 'the “show” command'
 
 import re
-import datetime
 
 import lxml.html
 
 from lib import colors
 from lib import dotparser
 from lib import indent
-from lib import soapbar
+from lib import debsoap
 
 def print_version_graph(graph, *, ilevel=0):
     vcolors = dict(
@@ -73,89 +72,67 @@ def run(options):
     response.raise_for_status()
     html = lxml.html.fromstring(response.text)
     html.make_links_absolute(base_url=url)
-    soapclient = soapbar.Client(session=session, url='https://bugs.debian.org/cgi-bin/soap.cgi', ns='Debbugs/SOAP')
-    bug_status = soapclient.get_status(bugno)
-    def sget(name):
-        return bug_status.find('.//{Debbugs/SOAP}' + name).text
-    def sget_list(name):
-        xp = './/d:' + name + '/d:item/text()'
-        return bug_status.xpath(xp, namespaces=dict(d='Debbugs/SOAP'))
-    def sget_int_list(name):
-        s = bug_status.find('.//{Debbugs/SOAP}' + name).text or ''
-        return [int(x) for x in s.split()]
-    print('Subject: {colors.bold}{subject}{colors.off}'.format(subject=sget('subject'), colors=colors))
-    package = sget('package')
-    if package.startswith('src:'):
-        source = package[4:]
-        print('Source: {colors.bold}{pkg}{colors.off}'.format(pkg=source, colors=colors))
+    debsoap_client = debsoap.Client(session=session)
+    status = debsoap_client.get_status(bugno)
+    print('Subject: {colors.bold}{subject}{colors.off}'.format(subject=status.subject, colors=colors))
+    if status.package.startswith('src:'):
+        print('Source: {colors.bold}{pkg}{colors.off}'.format(pkg=status.package[4:], colors=colors))
     else:
-        print('Package: {colors.bold}{pkg}{colors.off}'.format(pkg=package, colors=colors))
-        source = sget('source')
-        if source is not None:
-            print('Source: {pkg}'.format(pkg=source, colors=colors))
-    affects = (sget('affects') or '').split()
-    if affects:
+        print('Package: {colors.bold}{pkg}{colors.off}'.format(pkg=status.package, colors=colors))
+        if status.source is not None:
+            print('Source: {pkg}'.format(pkg=status.source, colors=colors))
+    if status.affects:
         print('Affects:')
-        for apkg in affects:
+        for apkg in status.affects:
             print('  {pkg}'.format(pkg=apkg))
     # TODO: Maintainer
-    owner = sget('owner')
-    if owner:
-        print('Owner: {user}'.format(user=owner))
-    submitter = sget('originator')
-    if package == 'wnpp' and owner == submitter:
+    if status.owner:
+        print('Owner: {user}'.format(user=status.owner))
+    if status.package == 'wnpp' and status.owner == status.submitter:
         pass
     else:
-        print('Submitter: {user}'.format(user=submitter))
-    date = int(sget('date'))
-    date = datetime.datetime.utcfromtimestamp(date)
-    print('Date: {date} UTC'.format(date=date))
-    severity = sget('severity')
+        print('Submitter: {user}'.format(user=status.submitter))
+    print('Date: {date} UTC'.format(date=status.date))
     severity_color = (
-        (colors.bold + colors.red) if severity in rc_severities
+        (colors.bold + colors.red) if status.severity in rc_severities
         else colors.off
     )
-    print('Severity: {color}{severity}{colors.off}'.format(severity=severity, color=severity_color, colors=colors))
+    print('Severity: {color}{severity}{colors.off}'.format(
+        severity=status.severity,
+        color=severity_color, colors=colors)
+    )
     version_graph = extract_bug_version_graph(html, options=options)
-    tags = sget('tags')
-    if tags:
-        print('Tags: {tags}'.format(tags=tags))
-    merged_with = sget_int_list('mergedwith')
-    if merged_with:
+    if status.tags:
+        print('Tags: {tags}'.format(tags=' '.join(status.tags)))
+    if status.merged_with:
         print('Merged-with:')
-        for mbug in merged_with:
+        for mbug in status.merged_with:
             print('  https://bugs.debian.org/{N}'.format(N=mbug))
-    found_versions = sget_list('found_versions')
-    if found_versions:
+    if status.found_versions:
         print('Found:')
-        for version in found_versions:
+        for version in status.found_versions:
             print('  {ver}'.format(ver=version))
-    fixed_versions = sget_list('fixed_versions')
-    if fixed_versions:
+    if status.fixed_versions:
         print('Fixed:')
-        for version in fixed_versions:
+        for version in status.fixed_versions:
             print('  {ver}'.format(ver=version))
     if version_graph:
         print('Version-Graph:')
         print_version_graph(version_graph, ilevel=2)
-    done = sget('done')
-    blocked_by = sget_int_list('blockedby')
-    if blocked_by:
+    if status.blocked_by:
         print('Blocked-by:')
-        for bbug in blocked_by:
+        for bbug in status.blocked_by:
             print('  https://bugs.debian.org/{N}'.format(N=bbug))
-    blocks = sget_int_list('blocks')
-    if blocks:
+    if status.blocks:
         print('Blocks:')
-        for bbug in blocks:
+        for bbug in status.blocks:
             print('  https://bugs.debian.org/{N}'.format(N=bbug))
-    if done:
-        print('Done: {user}'.format(user=done))
-    if int(sget('archived')):
+    if status.done:
+        print('Done: {user}'.format(user=status.done))
+    if status.archived:
         print('Archived: yes')
-    forwarded = sget('forwarded')
-    if forwarded:
-        print('Forwarded: {url}'.format(url=forwarded))
+    if status.forwarded:
+        print('Forwarded: {url}'.format(url=status.forwarded))
 
 __all__ = ['run']
 
---
+++ lib/debsoap.py
@@ -0,0 +1,129 @@
+# Copyright © 2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+Debian BTS SOAP client
+'''
+
+import datetime
+
+from lib import soapbar
+
+class BugStatus(object):
+
+    def __init__(self, xml):
+        self._xml = xml
+
+    @property
+    def subject(self):
+        return self._get('subject')
+
+    @property
+    def package(self):
+        return self._get('package')
+
+    @property
+    def source(self):
+        return self._get('source')
+
+    @property
+    def affects(self):
+        return (self._get('affects') or '').split()
+
+    @property
+    def owner(self):
+        return self._get('owner')
+
+    @property
+    def submitter(self):
+        return self._get('originator')
+
+    @property
+    def date(self):
+        ts = int(self._get('date'))
+        return datetime.datetime.utcfromtimestamp(ts)
+
+    @property
+    def severity(self):
+        return self._get('severity')
+
+    @property
+    def tags(self):
+        return (self._get('tags') or '').split()
+
+    @property
+    def merged_with(self):
+        return self._get_int_list('mergedwith')
+
+    @property
+    def found_versions(self):
+        return self._get_list('found_versions')
+
+    @property
+    def fixed_versions(self):
+        return self._get_list('fixed_versions')
+
+    @property
+    def blocked_by(self):
+        return self._get_int_list('blockedby')
+
+    @property
+    def blocks(self):
+        return self._get_int_list('blocks')
+
+    @property
+    def done(self):
+        return self._get('done')
+
+    @property
+    def archived(self):
+        return bool(int(self._get('archived')))
+
+    @property
+    def forwarded(self):
+        return self._get('forwarded')
+
+    def _get(self, name):
+        return self._xml.find('.//{Debbugs/SOAP}' + name).text
+
+    def _get_list(self, name):
+        xp = './/d:' + name + '/d:item/text()'
+        return self._xml.xpath(xp, namespaces=dict(d='Debbugs/SOAP'))
+
+    def _get_int_list(self, name):
+        s = self._xml.find('.//{Debbugs/SOAP}' + name).text or ''
+        return [int(x) for x in s.split()]
+
+class Client(object):
+
+    def __init__(self, *, session):
+        self._backend = soapbar.Client(
+            session=session,
+            url='https://bugs.debian.org/cgi-bin/soap.cgi',
+            ns='Debbugs/SOAP'
+        )
+
+    def get_status(self, n):
+        xml = self._backend.get_status(n)
+        return BugStatus(xml)
+
+__all__ = ['BugStatus', 'Client']
+
+# vim:ts=4 sts=4 sw=4 et
